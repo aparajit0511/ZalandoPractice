4e073cdd7592214a818f2781c06f4f736958935e
aparajit0511 <aparajit35@gmail.com>
2024-10-18 15:06:16 +0530

    deadlock demo2

--- src/DeadLockDemo/Main.java
+++ src/DeadLockDemo/Main.java
@@ -8,37 +8,38 @@ public class Main {
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                synchronized (lock1){
+                synchronized (lock1) {
                     System.out.println("Lock1 is locked by Thread1");
-                    try{
+                    try {
                         Thread.sleep(100);
-                    }catch(InterruptedException e){
+                    } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
                     System.out.println("thread1 trying to lock Lock2");
-
-                    synchronized (lock2){
+                } // close lock1 here to prevent deadlock
+                    synchronized (lock2) {
                         System.out.println("Lock2 is locked Thread1");
                     }
-                }
+                // close lock1 here to create deadlock
 
             }
         });
 
         Thread t2 = new Thread(
                 ()->{
-                    synchronized (lock2){
+                    synchronized (lock2) {
                         System.out.println("Lock2 is locked by Thread2");
-                        try{
+                        try {
                             Thread.sleep(100);
-                        }catch(InterruptedException e){
+                        } catch (InterruptedException e) {
                             throw new RuntimeException(e);
                         }
                         System.out.println("Thread2 is trying to lock lock1");
-                        synchronized (lock1){
+                    }// close lock2 here to prevent deadlock
+                        synchronized (lock1) {
                             System.out.println("Lock1 is locked by Thread2");
                         }
-                    }
+                    //  close lock2 here to create deadlock
                 }
         );
 
